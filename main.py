42cef38b77973343a7719fdcd2af4cbca0a19d52
Glenn <pounguiglennstephen@gmail.com>
2022-10-11 14:30:49 +0100

    fin du projet

--- main.py
+++ main.py
@@ -1,4 +1,51 @@
 import youtube_downloader
 
-url = r"https://www.youtube.com/watch?v=BhZXw32pNVQ&ab_channel=L%E2%80%99OiseauRare8G"
+BASE_YOUTUBE = "https://www.youtube.com/"
 
+urls = []
+while True:
+    url = input("Veillez entrer le lien à télécharger: ")
+    if url == "":
+        print("Veillez entrer un lien !")
+        print()
+    elif not url.lower().startswith(BASE_YOUTUBE):
+        print("ERREUR: Veillez entrer un lien youtube !")
+        print()
+    else:
+        urls.append(url)
+        while True:
+            add_url = input("Souhaitez-vous ajouter un autre lien (y/n) ? ")
+            if add_url.lower() == "n":
+                leave = True
+                break
+            elif add_url.lower() == "y":
+                leave = False
+                break
+            else:
+                print("Veillez répondre par 'y' ou 'n' !")
+                print()
+        if leave:
+            break
+
+while True:
+    choice = input("""
+    Voulez-vous télécharger en audio ou en vidéo ?
+    1 - Audio
+    2 - Vidéo
+
+    Votre réponse: """)
+
+    if choice == "1" or choice.lower() == "audio":
+        for url in urls:
+            youtube_downloader.download_audio(url)
+            leave = True
+            break
+    elif choice == "2" or choice.lower() == "vidéo":
+        for url in urls:
+            youtube_downloader.download_video(url)
+            leave = True
+            break
+    else:
+        print("Veillez faire un choix entre un et deux !")
+    if leave:
+        break
--- youtube_downloader.py
+++ youtube_downloader.py
@@ -11,15 +11,31 @@ def on_download_progress(stream, chunk, bytes_remaining):
     
 def download_video(url):
     youtube_video = YouTube(url)
+
     youtube_video.register_on_progress_callback(on_download_progress)
+
     streams = youtube_video.streams.filter(progressive=False, file_extension='mp4', type="video").order_by('resolution').desc()
     stream_video = streams[0]
     streams = youtube_video.streams.filter(progressive=False, file_extension='mp4', type="audio").order_by('abr').desc()
     stream_audio = streams[0]
+
+    print(f"Titre: {stream_video.title}")
+    print(f"Taille: {stream_video.filesize_mb + stream_audio.filesize_mb}")
     print("Téléchargement...")
-    stream_video.download()
-    stream_audio.download()
+    stream_video.download("video")
+    stream_audio.download("audio")
+
+    video_filename = os.path.join("video", stream_video.default_filename)
+    audio_filename = os.path.join("audio", stream_audio.default_filename)
+    output_filename = stream_video.default_filename
+
+    ffmpeg.output(ffmpeg.input(audio_filename), ffmpeg.input(video_filename), output_filename, vcodec="copy", acodec="copy", loglevel="quiet").run(overwrite_output=True)
     print("OK")
+
+    os.remove(audio_filename)
+    os.remove(video_filename)
+    os.rmdir("audio")
+    os.rmdir("video")
 
 
 def download_audio(url):
@@ -27,6 +43,8 @@ def download_audio(url):
     youtube_video.register_on_progress_callback(on_download_progress)
     streams = youtube_video.streams.filter(progressive=False, file_extension='mp4', type="audio").order_by('abr').desc()
     stream_audio = streams[0]
+    print(f"Titre: {stream_audio.title}")
+    print(f"Taille: {stream_audio.filesize_mb}")
     print("Téléchargement...")
     stream_audio.download()
     print("OK")
@@ -34,33 +52,7 @@ def download_audio(url):
 
 if __name__ == "__main__":
 
-    check_url("https://www.yutube.com/watch?v=BhZXw32pNVQ&ab_channel=L%E2%80%99OiseauRare8G")
+    download_video("https://www.youtube.com/watch?v=zBjJUV-lzHo&ab_channel=MEMESHUB")
     
-    """
-    # Récupérer le lien de téléchargement
-    url = r"https://www.youtube.com/watch?v=BhZXw32pNVQ&ab_channel=L%E2%80%99OiseauRare8G"
-    # url = get_video_url_from_user()
-
-    
-
-    
-
-    print(f"TITRE : {youtube_video.title}")
-    print(f"NB VUES : {youtube_video.views}")
-
-    print("")
-    print("CHOIX DES RESOLUTIONS")
-    
-
-    
-
-    print(f"STREAM Video: {stream_video}")
-    print(f"STREAM Audio: {stream_audio}")
-
-    # itag = get_video_stream_itag_from_user(streams)
-
-    # stream = youtube_video.streams.get_by_itag(itag)
-    
-    """
 
 
